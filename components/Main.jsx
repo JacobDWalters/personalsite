7babd3f4ac001e671d93bd4c016f7de9de324161
Jacob <jacob.d.walters22@gmail.com>
2022-12-08 23:08:40 -0500

    first commit of site

---
+++ components/Main.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {AiOutlineClose, AiOutlineMail, AiOutlineMenu} from 'react-icons/ai';
+import {FaGithub, FaLinkedinIn} from 'react-icons/fa';
+import {BsFillPersonLinesFill} from 'react-icons/bs';
+
+const Main = () => {
+  return (
+    <div className='w-full h-screen text-center'>
+        <div className='max-w-[1240px w-full h-full mx-auto p-2 flex justify-center items-center' >
+            <div>
+                <p className='uppercase text-sm tracking-widest text-gray-600'>Some Cool Saying when I am less Dumb</p>
+                <h1 className='py-4 text-gray-700'>
+                    Hi, I'm 
+                    <span className='text-[#5b7379]'> Jake</span>
+                </h1>
+                <h1 className='py-2 text-gray-700'>And I Suck At This A Lot!</h1>
+                <p className='py-4 text-gray-600 max-w-[70%] m-auto'>This will be my summary of who I am and what I do and why you should want me to want you, and need me to need you, and love me to love you and all that good and great stuff and give me a job</p>
+                <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                  <FaLinkedinIn />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                  <FaGithub />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                  <AiOutlineMail />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                  <BsFillPersonLinesFill />
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Main
